62d0d0748f95be9f5f1c9b2c5d1b37aa65a0d9f1
park hyunbin <wise7231@gmail.com>
2022-05-24 00:45:41 +0900

    .

--- app3.py
+++ app3.py
@@ -15,7 +15,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
-def getData(skins):
+def getCf(skins):
     dir = '화장품 추천시스템/최종데이터/'
     df_product = pd.read_csv(dir + 'basic_data_img.csv', usecols=['00.상품코드','00.상품_URL','00.이미지_URL','01.브랜드','02.상품명','03.가격','04.제품 주요 사양','05.모든 성분','06.총 평점','07.리뷰 개수','08_1.별점 1점','08_2.별점 2점','08_3.별점 3점','08_4.별점 4점','08_5.별점 5점','09_1.피부타입_건성','09_2.피부타입_복합성','09_3.피부타입_지성','10_1.피부고민_보습','10_2.피부고민_진정','10_3.피부고민_주름/미백','11_1.피부자극_없음','11_2.피부자극_보통','11_3.피부자극_있음'],encoding='cp949')
     df_review = pd.read_csv(dir + 'total_review.csv', usecols=['code','user','type','tone','problem','rating','feature','review','total_rating'],encoding='cp949')
@@ -148,6 +148,161 @@ def getData(skins):
     
     return result_dict
 
+def getCbf(skins):
+    dir = '화장품 추천시스템/최종데이터/'
+    df_product = pd.read_csv(dir + 'basic_data_img.csv', usecols=['00.상품코드','00.상품_URL','00.이미지_URL','01.브랜드','02.상품명','03.가격','04.제품 주요 사양','05.모든 성분','06.총 평점','07.리뷰 개수','08_1.별점 1점','08_2.별점 2점','08_3.별점 3점','08_4.별점 4점','08_5.별점 5점','09_1.피부타입_건성','09_2.피부타입_복합성','09_3.피부타입_지성','10_1.피부고민_보습','10_2.피부고민_진정','10_3.피부고민_주름/미백','11_1.피부자극_없음','11_2.피부자극_보통','11_3.피부자극_있음'],encoding='cp949')
+    df_review = pd.read_csv(dir + 'total_review.csv', usecols=['code','user','type','tone','problem','rating','feature','review','total_rating'],encoding='cp949')
+    
+    product_list = df_review['00.상품코드']
+    product_list = product_list.unique()
+    len(product_list)
+    
+    df_review.columns = ['code','user','type','tone','problem','rating','feature','review']
+    df_review = df_review.dropna(axis=0)
+    
+    df_product.columns = ['code','00.상품_URL','00.이미지_URL','01.브랜드','02.상품명','03.가격','04.제품 주요 사양','05.모든 성분','06.총 평점','07.리뷰 개수','08_1.별점 1점','08_2.별점 2점','08_3.별점 3점','08_4.별점 4점','08_5.별점 5점','09_1.피부타입_건성','09_2.피부타입_복합성','09_3.피부타입_지성','10_1.피부고민_보습','10_2.피부고민_진정','10_3.피부고민_주름/미백','11_1.피부자극_없음','11_2.피부자극_보통','11_3.피부자극_있음']
+    df_product = df_product.dropna(axis=0)
+    df_product = df_product.drop_duplicates(['code'])
+    
+    user_list = df_review['code']
+    user_list = user_list.unique()
+    user_list[0]
+    df_review_user = df_review[df_review['code']==user_list[0]]
+
+    problem_dict = {}
+    for problem in df_review_user['problem']:
+        problem_list = problem.split('|')
+        for i in problem_list:
+            i = i.replace(" ","")
+            if i in problem_dict:
+                problem_dict[i] += 1
+            else:
+                problem_dict[i] = 1
+    problem_dict_sort = sorted(problem_dict.items(),key = lambda x: x[1],reverse = True)
+    problem_dict_sort[0][0]
+    
+    user_list = df_review['code']
+    user_list = user_list.unique()
+    user_list[0]
+    df_review_user = df_review[df_review['code']==user_list[0]]
+
+    #최다빈출 type
+    type_freq = df_review_user['type'].value_counts()
+    #최다빈출 tone
+    tone_freq = df_review_user['tone'].value_counts()
+    #최다빈출 problem
+    problem_dict = {}
+    for problem in df_review_user['problem']:
+        problem_list = problem.split('|')
+        for i in problem_list:
+            i = i.replace(" ","")
+            if i in problem_dict:
+                problem_dict[i] += 1
+            else:
+                problem_dict[i] = 1
+    problem_dict_sort = sorted(problem_dict.items(),key = lambda x: x[1],reverse = True)
+
+
+
+    type = type_freq.index[0]
+    tone = tone_freq.index[0]
+    problem = ''
+    for i, problems in enumerate(problem_dict_sort):
+        problem += problems[0] + ' '
+        if i==20:
+            break
+    df_feature = pd.DataFrame(columns = ['code','feature'])
+
+
+    product_list = df_review['code']
+    product_list = product_list.unique()
+    index = 0
+    for product in product_list:
+        df_review_user = df_review[df_review['code']==product]
+        
+        #최다빈출 type
+        type_freq = df_review_user['type'].value_counts()
+        #최다빈출 tone
+        tone_freq = df_review_user['tone'].value_counts()
+        #최다빈출 problem
+        problem_dict = {}
+        for problem in df_review_user['problem']:
+            problem_list = problem.split('|')
+            for i in problem_list:
+                i = i.replace(" ","")
+                if i in problem_dict:
+                    problem_dict[i] += 1
+                else:
+                    problem_dict[i] = 1
+        problem_dict_sort = sorted(problem_dict.items(),key = lambda x: x[1],reverse = True)
+
+
+
+        type = type_freq.index[0]
+        tone = tone_freq.index[0]
+        problem = ''
+        for i, problems in enumerate(problem_dict_sort):
+            problem += problems[0] + ' '
+            if i==4:
+                break
+
+        feature = type+' '+tone+' '+problem
+        
+        df_feature.loc[str(index)] = [product,feature]
+    
+        index += 1
+        
+    df_feature.loc[str(index)] = ('input','건성 웜톤 트러블 미백 주름')
+    
+    df_feature.to_csv(dir+'df_feature.csv',header = True, index = True,encoding='cp949')
+    
+    counter_vector = CountVectorizer(ngram_range=(1,3))
+    c_vector_features = counter_vector.fit_transform(df_feature['feature'])
+    c_vector_features.shape
+    
+    similarity_feature = cosine_similarity(c_vector_features,c_vector_features).argsort()[:,::-1]
+    print(similarity_feature)
+    print(similarity_feature.shape)
+    
+    def recommend_product_list(df_feature, code , top=11):
+        #특정 제품코드 뽑아내기
+        target_feature_index = df_feature[df_feature['code'] == code].index.values
+
+        #타켓제품과 비슷한 코사인 유사도값
+        sim_index = similarity_feature[int(target_feature_index),:top].reshape(-1)
+        #본인제외
+        sim_index = sim_index[sim_index!=target_feature_index]
+        
+        #추천 결과 새로운 df 생성, 평균평점(score)으로 정렬
+        result = df_feature.iloc[sim_index]
+        
+        return result
+    
+    df_result = recommend_product_list(df_feature, code='input')
+    
+    df_recommend = pd.merge(df_product,df_result,on='code',how='right')
+    df_recommend
+
+    result_dict={}
+    products_dict = {}
+
+    for index in df_recommend.index:
+        if index == 0:
+            continue
+            
+        product_dict = {}
+        product_dict['productURL'] = str(df_recommend.loc[index]['00.상품_URL'])
+        product_dict['imageURL'] = str(df_recommend.loc[index]['00.이미지_URL'])
+        product_dict['brand'] = str(df_recommend.loc[index]['01.브랜드'])
+        product_dict['productName'] = str(df_recommend.loc[index]['02.상품명'])
+        product_dict['price'] = int(df_recommend.loc[index]['03.가격'])
+
+        products_dict[index] = product_dict
+        
+    result_dict['CBF'] = products_dict
+
+    return result_dict
+
 @app.route('/cf')
 def cf():
     skin_type = request.args.get('skintype')
@@ -159,7 +314,20 @@ def cf():
     for skin_worries in skin_worries:
         skins = skins + ' ' + skin_worries
         
-    return getData(skins)
+    return getCf(skins)
+
+@app.route('/cbf')
+def cbf():
+    skin_type = request.args.get('skintype')
+    skin_tone = request.args.get('skintone')
+    skin_worries = request.args.getlist('skinworry')
+    
+    skins = skin_type + ' ' + skin_tone
+    
+    for skin_worries in skin_worries:
+        skins = skins + ' ' + skin_worries
+        
+    return getCbf(skins)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3000, debug=True)
